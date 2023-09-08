8d1a58991a0ee5d4ee533504d73f4bac74c86958
Denis Mukhametov <3131467+denexapp@users.noreply.github.com>
2023-09-08 14:01:43 +0300

    Update links

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -6,43 +6,35 @@ import Title from '../Title';
 import Credits from '../Credits';
 import { Color } from '../Panel';
 
-type LinkData = [string, Color, string?, string?];
+type LinkData = [string, Color, string];
 
 const data: Array<LinkData> = [
   ['Telegram', [0, 136, 204], 'https://t.me/denexapp'],
-  ['GitHub', [36, 41, 46], 'https://github.com/denexapp'],
-  ['Twitter', [29, 160, 241], 'https://twitter.com/denexapp'],
-  ['VK', [70, 128, 194], 'https://vk.com/denexapp'],
+  ['Discord', [114, 137, 218], 'https://discord.com/users/304173216828489729']
   ['Instagram', [215, 0, 89], 'https://www.instagram.com/denexapp/'],
+  ['GitHub', [36, 41, 46], 'https://github.com/denexapp'],
   ['YouTube', [255, 0, 0], 'https://youtube.com/denexapp'],
   ['Twitch', [100, 65, 164], 'https://twitch.tv/denexapp'],
   ['Steam', [0, 0, 0], 'https://steamcommunity.com/id/denexapp'],
-  ['Battle.net', [4, 167, 242], undefined, 'denexapp#2391'],
-  ['Discord', [114, 137, 218], undefined, 'denexapp#0001']
+  ['Battle.net', [4, 167, 242], 'https://overwatch.blizzard.com/en-us/career/denexapp-2391'],
+  ['VRChat', [11, 90, 149], 'https://vrchat.com/home/user/usr_47ef8889-194d-4834-ab0a-33a90d9fc89c'],
+  ['Reddit', [255, 86, 0], 'https://www.reddit.com/user/denexapp']
+  ['VK', [70, 128, 194], 'https://vk.com/denexapp'],
+  ['Twitter', [29, 160, 241], 'https://twitter.com/denexapp'],
 ];
+
+const links = data.map(([name, color, url]) => (
+  <Link color={color} url={url}>
+    {name}
+  </Link>
+));
 
 export default () => (
   <div className={styles.component}>
     <div className={styles.spacer} />
-
-    <Title>Denis Mukhametov</Title>
+    <Title>denexapp</Title>
     <div className={styles.container}>
-      {data.map(([name, color, url, copy]) => {
-        if (typeof url === 'string') {
-          return (
-            <Link color={color} url={url}>
-              {name}
-            </Link>
-          );
-        } else if (typeof copy === 'string') {
-          return (
-            <Button color={color} textToCopy={copy}>
-              {name}
-            </Button>
-          );
-        }
-        return null;
-      })}
+      {links}
     </div>
     <div className={styles.spacer} />
     <Credits link="https://unsplash.com/photos/R4Y3JSKXfio">
