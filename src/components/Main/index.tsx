a359a14015ab17804bb12f43c8f6924c03e0677f
Denis Mukhametov <3131467+denexapp@users.noreply.github.com>
2023-09-08 14:08:39 +0300

    Update links

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -13,6 +13,7 @@ const data: Array<LinkData> = [
   ['Discord', [114, 137, 218], 'https://discord.com/users/304173216828489729']
   ['Instagram', [215, 0, 89], 'https://www.instagram.com/denexapp/'],
   ['GitHub', [36, 41, 46], 'https://github.com/denexapp'],
+  ['Ethereum', [135, 169, 239], 'https://etherscan.io/address/0x9F48c5f58903c66006A0432231b9a9ac0aE3Da61']
   ['YouTube', [255, 0, 0], 'https://youtube.com/denexapp'],
   ['Twitch', [100, 65, 164], 'https://twitch.tv/denexapp'],
   ['Steam', [0, 0, 0], 'https://steamcommunity.com/id/denexapp'],
@@ -20,7 +21,6 @@ const data: Array<LinkData> = [
   ['VRChat', [11, 90, 149], 'https://vrchat.com/home/user/usr_47ef8889-194d-4834-ab0a-33a90d9fc89c'],
   ['Reddit', [255, 86, 0], 'https://www.reddit.com/user/denexapp']
   ['VK', [70, 128, 194], 'https://vk.com/denexapp'],
-  ['Twitter', [29, 160, 241], 'https://twitter.com/denexapp'],
 ];
 
 const links = data.map(([name, color, url]) => (
